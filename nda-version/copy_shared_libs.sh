3eee9c79f8b6f0ec72d7f56aa0431df4379729ab
Ian Glen Neal <ian.gl.neal@gmail.com>
2019-06-13 14:31:17 -0400

    Remove the comparative version thing for now.

--- nda-version/copy_shared_libs.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-LIBC_PATH=$(realpath ../libc/glibc/build/install/lib)
-SOS=$(ldd bin/* | grep "=>" | cut -f 2 | cut -d" " -f 3 | grep -v "/home" | sort | uniq | grep -v "libc" | grep -v "ld-linux")
-for so in $SOS; do
-  cp -v $so $LIBC_PATH
-done
