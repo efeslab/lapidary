aaa687acb42776a77f1222921d64bb02e4a13c42
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-01-14 16:51:38 -0500

    Turns out, rather than using a custom action, we can use just the type if we have a single argument constructor (essentially a type cast)

--- lapidary/config/LapidaryConfig.py
+++ lapidary/config/LapidaryConfig.py
@@ -30,16 +30,6 @@ class LapidaryConfigHelp(Action):
         print(yaml.dump(get_schema()))
         exit(0)
 
-class LapidaryConfigHandler(Action):
-    def __init__(self, option_strings, dest, **kwargs):
-        super().__init__(option_strings, dest, **kwargs)
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        filename = getattr(namespace, self.dest)
-        config = self.type(filename=filename)
-        setattr(namespace, self.dest, config)
-        Gem5FlagConfig.parse_plugins(config)
-
 class LapidaryConfig(dict):
 
     def _parse_yaml_data(self, schema, raw_config):
